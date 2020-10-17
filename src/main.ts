d25889f663218a134babeecc1d8f246677a0d30f
Roniel Valdez <roniel_valdez@outlook.com>
2020-10-17 19:28:40 -0400

    add more https config

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,17 @@ async function bootstrap() {
 
   if (env === 'production') {
     const httpsOptions = {
-      key: fs.readFileSync('./secrets/private-key.pem'),
-      cert: fs.readFileSync('./secrets/public-certificate.pem'),
+      key: fs.readFileSync('./secrets/private.key', 'utf8'),
+      cert: fs.readFileSync('./secrets/certificate.crt', 'utf8'),
     };
 
     app = await NestFactory.create(AppModule, {
       httpsOptions,
     });
-
   } else {
     app = await NestFactory.create(AppModule);
-    app.enableCors();
   }
+  app.enableCors();
 
   await app.listen(port);
 }
